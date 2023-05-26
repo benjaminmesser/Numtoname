0ea1225f7052ce08bfd87c5c4c719c46ef3c02e7
Benjamin Messer <benjaminmesser@outlook.com>
2023-05-25 18:07:14 -0600

    small mistake

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: MIT License',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
   ],
 )
